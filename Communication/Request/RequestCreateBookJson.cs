e9d0771b0cf2f28eb39c140263575acf2d1349ac
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2026-01-20 12:29:03 -0300

    Validações dos dados de entrada na classe Request

--- Communication/Request/RequestCreateBookJson.cs
+++ Communication/Request/RequestCreateBookJson.cs
@@ -1,12 +1,27 @@
 ﻿using LivrariaApi.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace LivrariaApi.Communication.Request;
 
 public class RequestCreateBookJson
 {
+    [Required(ErrorMessage = "O título é obrigatório.")]
+    [StringLength(120, MinimumLength = 2, ErrorMessage = "O título deve ter entre 2 e 120 caracteres.")]
     public string Title { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "O autor é obrigatório.")]
+    [StringLength(120, MinimumLength = 2, ErrorMessage = "O autor deve ter entre 2 e 120 caracteres.")]
     public string Author { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "O gênero é obrigatório.")]
+    [EnumDataType(typeof(Genre), ErrorMessage = "Gênero inválido.")]
     public Genre Genre { get; set; }
+
+    [Required(ErrorMessage = "O preço é obrigatório.")]
+    [Range(0, double.MaxValue, ErrorMessage = "O preço deve ser maior ou igual a zero.")]
     public decimal Price { get; set; } = decimal.Zero;
+
+    [Required(ErrorMessage = "O estoque é obrigatório.")]
+    [Range(0, int.MaxValue, ErrorMessage = "O estoque deve ser maior ou igual a zero.")]
     public int Stock { get; set; } = 0;
 }
